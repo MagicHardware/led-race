01f2d033a89d90c841bc9d1a58271f0dbc66deff
MagicHardware <57773835+MagicHardware@users.noreply.github.com>
2019-11-14 21:54:47 +0100

    Last Arduino_IDE project.

--- led_race3_5_AD19_SMS.ino
+++ led_race3_5_AD19_SMS.ino
@@ -8,7 +8,7 @@
         | |                                                             
         |_|          
  Open LED Race
- An minimalist cars race for LED strip  
+ An minimalist cars race for LED track  
   
  This program is free software; you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
@@ -25,18 +25,36 @@
 
                                                             
 #include <Adafruit_NeoPixel.h>
-#define MAXLED         300 // MAX LEDs actives on strip
+#define MAXLED         300 // MAX LEDs actives on track
 
 #define PIN_LED        A0  // R 500 ohms to DI pin for WS2812 and WS2813, for WS2813 BI pin of first LED to GND  ,  CAP 1000 uF to VCC 5v/GND,power supplie 5V 2A  
 #define PIN_P1         7   // switch player 1 to PIN and GND
 #define PIN_P2         6   // switch player 2 to PIN and GND 
+#define PIN_P3         5   // switch player 2 to PIN and GND 
 #define PIN_AUDIO      3   // through CAP 2uf to speaker 8 ohms
-
+#define Inacivity_time 30  //time to start disco in s
 
 int NPIXELS=MAXLED; // leds on track
 
-#define COLOR1    track.Color(255,0,0)
-#define COLOR2    track.Color(0,255,0)
+byte  gravity_map[MAXLED];  
+struct sColor_my{
+  int R;
+  int G;
+  int B;
+};
+
+
+struct sColor_my Color1={0,0,255};
+struct sColor_my Color2={255,255,0};
+struct sColor_my Color3={0,255,0};
+struct sColor_my Color_hill={127,127,127};
+
+#define CAR1_Color track.Color(Color1.R,Color1.G,Color1.B-i*20)
+#define CAR2_Color track.Color(Color2.R-i*20,Color2.G-i*20,Color2.B)
+#define CAR3_Color track.Color(Color3.R,Color3.G-i*20,Color3.B)
+#define CAR_HILL_Color track.Color(((Color_hill.R-gravity_map[i])/8 ), ((Color_hill.G-gravity_map[i])/8), ((Color_hill.B-gravity_map[i])/8))
+
+
 
 int win_music[] = {
   2637, 2637, 0, 2637, 
@@ -44,17 +62,21 @@ int win_music[] = {
   3136    
 };
       
-byte  gravity_map[MAXLED];     
+   
 
 int TBEEP=3; 
 
 float speed1=0;
 float speed2=0;
+float speed3=0;
+
 float dist1=0;
 float dist2=0;
+float dist3=0;
 
 byte loop1=0;
 byte loop2=0;
+byte loop3=0;
 
 byte leader=0;
 byte loop_max=5; //total laps race
@@ -66,11 +88,15 @@ float kg=0.003; //gravity constant
 
 byte flag_sw1=0;
 byte flag_sw2=0;
+byte flag_sw3=0;
 byte draworder=0;
  
-unsigned long timestamp=0;
+uint64_t  timestamp=0;
+uint64_t Best_timestamp=99999;
+uint64_t timestamp_push=0;
+uint8_t Block_return=0;
 
-Adafruit_NeoPixel track = Adafruit_NeoPixel(MAXLED, PIN_LED, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel track(MAXLED, PIN_LED, NEO_GRB + NEO_KHZ800);
 
 int tdelay = 5; 
 
@@ -88,25 +114,35 @@ void set_loop(byte H,byte a,byte b,byte c)
 
 
 void setup() {
+  Serial.begin(9600); 
+  
+  
   for(int i=0;i<NPIXELS;i++){gravity_map[i]=127;};
   track.begin(); 
+  track.setBrightness(255); //max 255
   pinMode(PIN_P1,INPUT_PULLUP); 
   pinMode(PIN_P2,INPUT_PULLUP);  
+  pinMode(PIN_P3,INPUT_PULLUP);  
 
-  if ((digitalRead(PIN_P1)==0)) //push switch 1 on reset for activate physic
-  {
-    set_ramp(12,90,100,110);    // ramp centred in LED 100 with 10 led fordward and 10 backguard 
-    for(int i=0;i<NPIXELS;i++){track.setPixelColor(i, track.Color(0,0,(127-gravity_map[i])/8) );};
+  //if ((digitalRead(PIN_P1)==0)) //push switch 1 on reset for activate physic
+  //{
+    set_ramp(5,35,45,80);    // ramp centred in LED 100 with 10 led fordward and 10 backguard
+    //set_ramp(17,90,130,160);
+  set_loop(5,90,135,160);
+    
+    for(int i=0;i<NPIXELS;i++){track.setPixelColor(i, CAR_HILL_Color );};
     track.show();
-  };
+  //};
+  
   start_race();    
 }
 
 void start_race(){for(int i=0;i<NPIXELS;i++){track.setPixelColor(i, track.Color(0,0,0));};
+                  loop1=0;loop2=0;loop3=0;dist1=0;dist2=0;dist3=0;speed1=0;speed2=0;speed3=0;timestamp=0;
                   track.show();
                   delay(2000);
-                  track.setPixelColor(12, track.Color(0,255,0));
-                  track.setPixelColor(11, track.Color(0,255,0));
+                  track.setPixelColor(12, track.Color(255,0,0));
+                  track.setPixelColor(11, track.Color(255,0,0));
                   track.show();
                   tone(PIN_AUDIO,400);
                   delay(2000);
@@ -121,13 +157,14 @@ void start_race(){for(int i=0;i<NPIXELS;i++){track.setPixelColor(i, track.Color(
                   noTone(PIN_AUDIO);                  
                   track.setPixelColor(9, track.Color(0,0,0));
                   track.setPixelColor(10, track.Color(0,0,0));
-                  track.setPixelColor(8, track.Color(255,0,0));
-                  track.setPixelColor(7, track.Color(255,0,0));
+                  track.setPixelColor(8, track.Color(0,255,0));
+                  track.setPixelColor(7, track.Color(0,255,0));
                   track.show();
                   tone(PIN_AUDIO,1200);
                   delay(2000);
                   noTone(PIN_AUDIO);                               
-                  timestamp=0;              
+                  timestamp=millis();
+                                
                  };
 
 void winner_fx() {
@@ -141,6 +178,108 @@ void winner_fx() {
 
                                                
               };
+
+void colorWipe(uint32_t color, int wait) {
+  for(int i=0; i<track.numPixels(); i++) { // For each pixel in track...
+    track.setPixelColor(i, color);         //  Set pixel's color (in RAM)
+    track.show();                          //  Update track to match
+    delay(wait);                           //  Pause for a moment
+           if( Block_return==1){return;   }else if((digitalRead(PIN_P1)==0) || (digitalRead(PIN_P2)==0) || (digitalRead(PIN_P3)==0)){start_race();Block_return=1;timestamp_push=millis(); return; }   
+  }
+}
+
+// Theater-marquee-style chasing lights. Pass in a color (32-bit value,
+// a la track.Color(r,g,b) as mentioned above), and a delay time (in ms)
+// between frames.
+void theaterChase(uint32_t color, int wait) {
+  for(int a=0; a<10; a++) {  // Repeat 10 times...
+    for(int b=0; b<3; b++) { //  'b' counts from 0 to 2...
+      track.clear();         //   Set all pixels in RAM to 0 (off)
+      // 'c' counts up from 'b' to end of track in steps of 3...
+      for(int c=b; c<track.numPixels(); c += 3) {
+        track.setPixelColor(c, color); // Set pixel 'c' to value 'color'
+               if( Block_return==1){return;   }else if((digitalRead(PIN_P1)==0) || (digitalRead(PIN_P2)==0) || (digitalRead(PIN_P3)==0)){start_race();Block_return=1;timestamp_push=millis(); return; }   
+      }
+      track.show(); // Update track with new contents
+      delay(wait);  // Pause for a moment
+    }
+  }
+}
+
+// Rainbow cycle along whole track. Pass delay time (in ms) between frames.
+void rainbow(int wait) {
+  // Hue of first pixel runs 5 complete loops through the color wheel.
+  // Color wheel has a range of 65536 but it's OK if we roll over, so
+  // just count from 0 to 5*65536. Adding 256 to firstPixelHue each time
+  // means we'll make 5*65536/256 = 1280 passes through this outer loop:
+  for(long firstPixelHue = 0; firstPixelHue < 5*65536; firstPixelHue += 256) {
+    for(int i=0; i<track.numPixels(); i++) { // For each pixel in track...
+      // Offset pixel hue by an amount to make one full revolution of the
+      // color wheel (range of 65536) along the length of the track
+      // (track.numPixels() steps):
+      int pixelHue = firstPixelHue + (i * 65536L / track.numPixels());
+      // track.ColorHSV() can take 1 or 3 arguments: a hue (0 to 65535) or
+      // optionally add saturation and value (brightness) (each 0 to 255).
+      // Here we're using just the single-argument hue variant. The result
+      // is passed through track.gamma32() to provide 'truer' colors
+      // before assigning to each pixel:
+      track.setPixelColor(i, track.gamma32(track.ColorHSV(pixelHue)));
+             if( Block_return==1){return;   }else if((digitalRead(PIN_P1)==0) || (digitalRead(PIN_P2)==0) || (digitalRead(PIN_P3)==0)){start_race();Block_return=1;timestamp_push=millis(); return; }   
+    }
+    track.show(); // Update track with new contents
+    delay(wait);  // Pause for a moment
+  }
+}
+
+// Rainbow-enhanced theater marquee. Pass delay time (in ms) between frames.
+void theaterChaseRainbow(int wait) {
+  int firstPixelHue = 0;     // First pixel starts at red (hue 0)
+  for(int a=0; a<30; a++) {  // Repeat 30 times...
+    for(int b=0; b<3; b++) { //  'b' counts from 0 to 2...
+      track.clear();         //   Set all pixels in RAM to 0 (off)
+      // 'c' counts up from 'b' to end of track in increments of 3...
+      for(int c=b; c<track.numPixels(); c += 3) {
+        // hue of pixel 'c' is offset by an amount to make one full
+        // revolution of the color wheel (range 65536) along the length
+        // of the track (track.numPixels() steps):
+        int      hue   = firstPixelHue + c * 65536L / track.numPixels();
+        uint32_t color = track.gamma32(track.ColorHSV(hue)); // hue -> RGB
+        track.setPixelColor(c, color); // Set pixel 'c' to value 'color'
+      }
+      track.show();                // Update track with new contents
+      delay(wait);                 // Pause for a moment
+      firstPixelHue += 65536 / 90; // One cycle of color wheel over 90 frames
+            if( Block_return==1){return;   }else if((digitalRead(PIN_P1)==0) || (digitalRead(PIN_P2)==0) || (digitalRead(PIN_P3)==0)){start_race();Block_return=1;timestamp_push=millis(); return; }   
+    }
+  }
+}
+
+
+void Disco_effect(){    //neopixel demo
+  // Fill along the length of the track in various colors...
+  colorWipe(track.Color(255,   0,   0), 10); // Red
+        if( Block_return==1){return;   }else if((digitalRead(PIN_P1)==0) || (digitalRead(PIN_P2)==0) || (digitalRead(PIN_P3)==0)){start_race();Block_return=1;timestamp_push=millis(); return; }   
+  
+  colorWipe(track.Color(  0, 255,   0), 10); // Green
+         if( Block_return==1){return;   }else if((digitalRead(PIN_P1)==0) || (digitalRead(PIN_P2)==0) || (digitalRead(PIN_P3)==0)){start_race();Block_return=1;timestamp_push=millis(); return; }   
+  colorWipe(track.Color(  0,   0, 255), 10); // Blue
+         if( Block_return==1){return;   }else if((digitalRead(PIN_P1)==0) || (digitalRead(PIN_P2)==0) || (digitalRead(PIN_P3)==0)){start_race();Block_return=1;timestamp_push=millis(); return; }   
+
+  // Do a theater marquee effect in various colors...
+  theaterChase(track.Color(127, 127, 127), 50); // White, half brightness
+         if( Block_return==1){return;   }else if((digitalRead(PIN_P1)==0) || (digitalRead(PIN_P2)==0) || (digitalRead(PIN_P3)==0)){start_race();Block_return=1;timestamp_push=millis(); return; }   
+  theaterChase(track.Color(127,   0,   0), 50); // Red, half brightness
+         if( Block_return==1){return;   }else if((digitalRead(PIN_P1)==0) || (digitalRead(PIN_P2)==0) || (digitalRead(PIN_P3)==0)){start_race();Block_return=1;timestamp_push=millis(); return; }   
+  theaterChase(track.Color(  0,   0, 127), 50); // Blue, half brightness
+
+  rainbow(10);             // Flowing rainbow cycle along the whole track
+         if( Block_return==1){return;   }else if((digitalRead(PIN_P1)==0) || (digitalRead(PIN_P2)==0) || (digitalRead(PIN_P3)==0)){start_race();Block_return=1;timestamp_push=millis(); return; }   
+  theaterChaseRainbow(50); // Rainbow-enhanced theaterChase variant
+  
+  if( Block_return==1){return;
+  }else if((digitalRead(PIN_P1)==0) || (digitalRead(PIN_P2)==0) || (digitalRead(PIN_P3)==0)){start_race();Block_return=1;timestamp_push=millis(); return; }
+  
+}
 
 void burning1(){
 //to do
@@ -233,17 +372,21 @@ int display_lcd_time(){
 
 
 
-void draw_car1(void){for(int i=0;i<=loop1;i++){track.setPixelColor(((word)dist1 % NPIXELS)+i, track.Color(0,255-i*20,0));};                   
+void draw_car1(void){for(int i=0;i<=loop1;i++){track.setPixelColor(((word)dist1 % NPIXELS)+i, CAR1_Color );};                   
   }
 
-void draw_car2(void){for(int i=0;i<=loop2;i++){track.setPixelColor(((word)dist2 % NPIXELS)+i, track.Color(255-i*20,0,0));};            
+void draw_car2(void){for(int i=0;i<=loop2;i++){track.setPixelColor(((word)dist2 % NPIXELS)+i, CAR2_Color );};            
+ }
+
+void draw_car3(void){for(int i=0;i<=loop3;i++){track.setPixelColor(((word)dist3 % NPIXELS)+i, CAR3_Color );};            
  }
   
 void loop() {
+    Block_return=0;
     //for(int i=0;i<NPIXELS;i++){track.setPixelColor(i, track.Color(0,0,0));};
-    for(int i=0;i<NPIXELS;i++){track.setPixelColor(i, track.Color(0,0,(127-gravity_map[i])/8) );};
+    for(int i=0;i<NPIXELS;i++){track.setPixelColor(i, CAR_HILL_Color );};
     
-    if ( (flag_sw1==1) && (digitalRead(PIN_P1)==0) ) {flag_sw1=0;speed1+=ACEL;};
+    if ( (flag_sw1==1) && (digitalRead(PIN_P1)==0) ) {flag_sw1=0;speed1+=ACEL;timestamp_push=millis();};
     if ( (flag_sw1==0) && (digitalRead(PIN_P1)==1) ) {flag_sw1=1;};
 
     if ((gravity_map[(word)dist1 % NPIXELS])<127) speed1-=kg*(127-(gravity_map[(word)dist1 % NPIXELS]));
@@ -252,37 +395,118 @@ void loop() {
     
     speed1-=speed1*kf; 
     
-    if ( (flag_sw2==1) && (digitalRead(PIN_P2)==0) ) {flag_sw2=0;speed2+=ACEL;};
+    if ( (flag_sw2==1) && (digitalRead(PIN_P2)==0) ) {flag_sw2=0;speed2+=ACEL;timestamp_push=millis();};
     if ( (flag_sw2==0) && (digitalRead(PIN_P2)==1) ) {flag_sw2=1;};
 
     if ((gravity_map[(word)dist2 % NPIXELS])<127) speed2-=kg*(127-(gravity_map[(word)dist2 % NPIXELS]));
     if ((gravity_map[(word)dist2 % NPIXELS])>127) speed2+=kg*((gravity_map[(word)dist2 % NPIXELS])-127);
         
     speed2-=speed2*kf; 
+
+    
+    if ( (flag_sw3==1) && (digitalRead(PIN_P3)==0) ) {flag_sw3=0;speed3+=ACEL;timestamp_push=millis();};
+    if ( (flag_sw3==0) && (digitalRead(PIN_P3)==1) ) {flag_sw3=1;};
+
+    if ((gravity_map[(word)dist3 % NPIXELS])<127) speed3-=kg*(127-(gravity_map[(word)dist3 % NPIXELS]));
+    if ((gravity_map[(word)dist3 % NPIXELS])>127) speed3+=kg*((gravity_map[(word)dist3 % NPIXELS])-127);
+        
+    speed3-=speed3*kf; 
+
         
     dist1+=speed1;
     dist2+=speed2;
+    dist3+=speed3;
 
-    if (dist1>dist2) {leader=1;} 
-    if (dist2>dist1) {leader=2;};
+    if (dist1>dist2 && dist2>dist3) {leader=1;} 
+    if (dist2>dist1 && dist2>dist3) {leader=2;};
+    if (dist3>dist1 && dist3>dist2) {leader=3;};
       
     if (dist1>NPIXELS*loop1) {loop1++;tone(PIN_AUDIO,600);TBEEP=2;};
     if (dist2>NPIXELS*loop2) {loop2++;tone(PIN_AUDIO,700);TBEEP=2;};
+    if (dist3>NPIXELS*loop3) {loop3++;tone(PIN_AUDIO,700);TBEEP=2;};
 
-    if (loop1>loop_max) {for(int i=0;i<NPIXELS;i++){track.setPixelColor(i, track.Color(0,255,0));}; track.show();
-                                                    winner_fx();loop1=0;loop2=0;dist1=0;dist2=0;speed1=0;speed2=0;timestamp=0;
-                                                    start_race();
-                                                   }
-    if (loop2>loop_max) {for(int i=0;i<NPIXELS;i++){track.setPixelColor(i, track.Color(255,0,0));}; track.show();
-                                                    winner_fx();loop1=0;loop2=0;dist1=0;dist2=0;speed1=0;speed2=0;timestamp=0;
-                                                    start_race();
-                                                   }
+    
+    if(timestamp_push+Inacivity_time*1000<millis())
+    {
+      Disco_effect();
+    }
+
+    unsigned long timestamp_end=0;
+    if (loop1>loop_max) //win 1
+    {
+      for(int i=0;i<NPIXELS;i++)
+      {
+        track.setPixelColor(i, track.Color(Color1.R,Color1.G,Color1.B));
+      }; 
+      track.show();
+      
+      timestamp_end=millis();
+      uint64_t czas_przejazdu=timestamp_end-timestamp;
+      Serial.println("Gracz 1(niebieski). Najlepszy wynik: " + String((uint32_t)Best_timestamp) + "[ms] Czas osiagniety: " + String((uint32_t)czas_przejazdu) +" [ms] \n");
+     
+      if(czas_przejazdu < Best_timestamp)
+      {
+        Serial.println("Pobiles rekord, twoj czas: "+ String((uint32_t)czas_przejazdu) +"[ms] \n");
+        Best_timestamp=czas_przejazdu;                                                    
+      }
+
+      
+      winner_fx();
+      start_race();
+    }
+    
+    if (loop2>loop_max) //win 2
+    {
+      for(int i=0;i<NPIXELS;i++)
+      {
+        track.setPixelColor(i, track.Color(Color2.R,Color2.G,Color2.B) );
+      }
+      track.show();
+      
+      timestamp_end=millis();
+      uint64_t czas_przejazdu=timestamp_end-timestamp;
+      Serial.println("Gracz 2(zolty). Najlepszy wynik: " + String((uint32_t)Best_timestamp) +"[ms] Czas osiagniety: " + String((uint32_t)czas_przejazdu) +"[ms] \n");
+     
+      if(czas_przejazdu < Best_timestamp)
+      {
+        Serial.println("Pobiles rekord, twoj czas: "+ String((uint32_t)czas_przejazdu) +"[ms] \n");
+        Best_timestamp=czas_przejazdu;                                                    
+      }
+
+      
+      winner_fx();
+      start_race();
+    }
+
+    if (loop3>loop_max) //win 2
+    {
+      for(int i=0;i<NPIXELS;i++)
+      {
+        track.setPixelColor(i, track.Color(Color3.R,Color3.G,Color3.B) );
+      }
+      track.show();
+      
+      timestamp_end=millis();
+      uint32_t czas_przejazdu=timestamp_end-timestamp;
+      Serial.println("Gracz 3(zielony). Najlepszy wynik: " + String((uint32_t)Best_timestamp) +"[ms] Czas osiagniety: " + String((uint32_t)czas_przejazdu) +"[ms] \n");
+     
+      if(czas_przejazdu < Best_timestamp)
+      {
+        Serial.println("Pobiles rekord, twoj czas: "+ String((uint32_t)czas_przejazdu) +"[ms] \n");
+        Best_timestamp=czas_przejazdu;                                                    
+      }
+
+      
+      winner_fx();
+      start_race();
+    }
+    
     if ((millis() & 512)==(512*draworder)) {if (draworder==0) {draworder=1;}
                           else {draworder=0;}   
                          }; 
 
-    if (draworder==0) {draw_car1();draw_car2();}
-        else {draw_car2();draw_car1();}   
+    if (draworder==0) {draw_car1();draw_car2();draw_car3();}
+        else {draw_car3(); draw_car2();draw_car1();}   
                  
     track.show(); 
     delay(tdelay);
